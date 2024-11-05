f7d0826764cccbdbfe6e18e45c9613281c78cd49
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-11-05 11:33:29 +0200

    changes

---
+++ meeting1/basics.py
@@ -0,0 +1,65 @@
+# print("Hello")
+# print("Bye")
+# num = 5
+#
+# def hello():
+#     print("Hello from function")
+#     print("ttttt")
+#
+# print("1111")
+# hello()
+#
+# hello = 5
+# print(hello)
+# print("222")
+# # hello()
+#
+
+num = 5
+print(type(num))
+
+print(num/2)
+
+word = "November"
+print(type(word))
+
+print(word*20)
+
+# print(word + 2)
+print(word + " " + "2")
+
+result = num / 5
+print(type(result))
+
+result = num * 5
+print(type(result))
+
+result = num * 5.
+print(type(result))
+
+my_bool = num > 9
+print(type(my_bool))
+
+b = True
+c = False
+
+print(b and c)
+
+print("5 and 0")
+print(5 and 0)
+print(5 and 6)
+print(0 and 5)
+
+# name = input("Insert your name: ")
+# print(name)
+# print(type(name))
+# year = input("Year: ")
+# age = 2024 - int(year)
+
+# print("You are age years old")
+# print("You are " + age + " years old")
+# print("You are " + str(age) + " years old")
+# print("You are", age, "years old")
+# print(f"Hi {name}, You are {age} years old")
+
+# print(f"Hi {input('your name: ')}, You are {2024 - int(input('your year: '))} years old")
--- meeting1/gradio_demo.py
+++ meeting1/gradio_demo.py
@@ -2,30 +2,30 @@ import gradio as gr
 from gradio.components.number import Number
 
 
-# def greet(name, intensity):
-#     return "Hello, " + name + "!" * int(intensity)
-#
-#
-# demo = gr.Interface(
-#     fn=greet,
-#     inputs=["text", "slider"],
-#     outputs=["text"],
-#     flagging_mode="never"
-# )
-
-
-def calc_area(width, height):
-    return f"The area is {width*height}"
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
 
 
 demo = gr.Interface(
-    fn=calc_area,
-    # inputs=["number", "number"],
-    inputs=[Number(label="Insert width:"), Number(label="Insert height")],
+    fn=greet,
+    inputs=["text", "slider"],
     outputs=["text"],
-    flagging_mode="never",
-    submit_btn="Calculate"
+    flagging_mode="never"
 )
 
 
-demo.launch()
+# def calc_area(width, height):
+#     return f"The area is {width*height}"
+#
+#
+# demo = gr.Interface(
+#     fn=calc_area,
+#     # inputs=["number", "number"],
+#     inputs=[Number(label="Insert width:"), Number(label="Insert height")],
+#     outputs=["text"],
+#     flagging_mode="never",
+#     submit_btn="Calculate"
+# )
+
+
+demo.launch(share=True)
