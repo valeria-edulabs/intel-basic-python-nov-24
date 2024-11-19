f53be4bbb5807ba392ab273d4eb0d733b61170b3
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-11-19 09:47:46 +0200

    changes

--- meeting3/while_loop.py
+++ meeting3/while_loop.py
@@ -1,13 +1,29 @@
-names_list = []
-name = ""
-while name != "$":
-    name = input("Insert a name: ")
-    names_list.append(name)
-names_list.pop()
-print("The names are:", names_list)
-print("Total names inserted:", len(names_list))
-total_chars = sum([len(word) for word in names_list])
+# names_list = []
+# name = ""
+# while name != "$":
+#     name = input("Insert a name: ")
+#     names_list.append(name)
+# names_list.pop()
+# print("The names are:", names_list)
+# print("Total names inserted:", len(names_list))
+# total_chars = sum([len(word) for word in names_list])
 # chars = []
 # for word in names_list:
 #     chars.append(len(word))
-print("Total characters: ", total_chars)
+# print("Total characters: ", total_chars)
+
+
+names_list = []
+while True:
+    name = input("Insert a name: ")
+    if name == "Dont know":
+        print("bla")
+        continue
+    if name != "$":
+        names_list.append(name)
+    else:
+        break
+
+print("The names are:", names_list)
+print("Total names inserted:", len(names_list))
+total_chars = sum([len(word) for word in names_list])
