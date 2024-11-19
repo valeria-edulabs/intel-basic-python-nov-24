18aafc4973cd77d85aeab7a68c70bd4610035a1b
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2024-11-19 11:15:10 +0200

    changes

---
+++ meeting3/dates_demo.py
@@ -0,0 +1,44 @@
+# bday = "11-02-1987"
+# result = bday.split("-")
+# print(result)
+# d, m, y = bday.split("-")
+# print(d)
+# today = "19-11-2024"
+import time
+from datetime import datetime,date, timedelta
+
+# datetime.datetime
+# datetime.date
+# datetime.time
+# datetime.timedelta
+
+print(datetime.now(), type(datetime.now()))
+print(date.today(), type(date.today()))
+
+ts1 = datetime.now()
+time.sleep(3)
+ts2 = datetime.now()
+
+print("ts1", ts1, "ts2", ts2)
+print(ts2 - ts1)
+
+yogev = datetime(1985, 8, 5, 15, 6, 4)
+valeria = datetime(1987, 2, 11, 20, 59, 7)
+
+
+
+print(valeria - yogev)
+print(yogev - valeria)
+
+
+now = datetime.now()
+print(now + timedelta(minutes=15))
+
+
+print(now.weekday())
+# Sun, 18-11-2024
+print(now.strftime("%a, %d-%m-%Y"))
+
+my_date = "01/06/1999 13:04:09"
+my_date_as_datetime = datetime.strptime(my_date, "%d/%m/%Y %H:%M:%S")
+print(datetime.now() - my_date_as_datetime)
---
+++ meeting3/dict_demo.py
@@ -0,0 +1,22 @@
+my_dict = {}
+countries = {
+    "Israel": "Jerusalem",
+    "US": "Washington"
+}
+countries["UK"] = "London"
+
+cities = ["Kiryat Gat", "Tel Aviv"]
+# cities[2] = "Jerusalem"
+cities.append("Jerusalem")
+print(countries["US"])
+
+citizens = {
+    123123123: "Valeria",
+    456456456: "Valeria"
+}
+
+person = {
+    "name": "Valeria",
+    "surname": "Aynbinder",
+    "b_year": 1987,
+}
